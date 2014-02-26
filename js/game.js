292a4e03ec3ee28566cbbe058de87324426d8f2e
Solomon Davidsohn <sol91325@yahoo.com>
2014-02-25 17:51:22 -0800

    Initial Controls

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
     //load with identifier and URL
@@ -9,7 +9,23 @@ function preload() {
 
 
 function create() {
-    //create sprite
-    game.add.sprite(0, 0, 'einstein');
 
+	//background
+	game.stage.backgroundColor = "#E0E4F1";
+	
+	//create sprite
+    testSprite = game.add.sprite(0, 0, 'einstein');
+	//sprite velocity 
+	testSprite.body.velocity.y = -10;
+
+	keyboard = game.input.keyboard; //create keyboard
+}
+
+function update(){
+    //keyboard control, press down button
+	if(keyboard.isDown(Phaser.Keyboard.DOWN)) {
+		testSprite.body.velocity.y = 20; //brings sprite down
+	} else {
+		testSprite.body.velocity.y = -10; //but only when pressing down
+	}
 }
